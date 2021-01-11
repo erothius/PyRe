b82d8726c8757c0bad2e3ea8fc1e0da04de303e9
erothius <erothiuslol@gmail.com>
2021-01-11 23:07:40 +0100

    sed-uprava

--- main.py
+++ main.py
@@ -3,14 +3,6 @@ import pandas as pd
 df1 = pd.read_excel("file_1.xlsx")
 df2 = pd.read_excel("file_2.xlsx")
 
-df1.insert = (2, "data1")
-df1.insert = (3, "data2")
+df3 = df1.merge(df2, on="cislo", how="inner")
 
-for x in df1["cislo"]:
-    for y in df2["cislo"]:
-        if x == y:
-            break
-
-
-print(df1)
-print(df2)
+print(df3)
